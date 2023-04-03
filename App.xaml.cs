56cbbfd1dfd1b46864851dac3aa3f3c7456f04cf
ihsansfd <ihsannurul@hotmail.com>
2023-04-03 08:11:07 +0700

    initial commit

---
+++ App.xaml.cs
@@ -0,0 +1,50 @@
+﻿using ExtendedPomodoro.ViewModels;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ExtendedPomodoro
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+
+        private ServiceProvider Services { get; }
+
+        public App()
+        {
+            Services = ConfigureServices();
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            var mainWindow = Services.GetRequiredService<MainWindow>();
+            mainWindow.DataContext = Services.GetRequiredService<MainWindowViewModel>();
+            mainWindow.Show();
+
+        }
+
+        private static ServiceProvider ConfigureServices()
+        {
+            var services = new ServiceCollection();
+            services.AddSingleton<MainWindowViewModel>();
+            services.AddSingleton<TimerViewModel>();
+            services.AddSingleton<MainWindow>();
+            services.AddSingleton<SettingsViewModel>();
+            services.AddSingleton<StatsViewModel>();
+            services.AddSingleton<TasksViewModel>();
+            services.AddSingleton<NavigationViewModel>((service) => new(service.GetRequiredService<TimerViewModel>()));
+
+            return services.BuildServiceProvider();
+        }
+    }
+}
