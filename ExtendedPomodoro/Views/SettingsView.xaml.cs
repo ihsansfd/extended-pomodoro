864c6760fb53bb284643128ff27bdb04b8d32456
ihsansfd <ihsannurul@hotmail.com>
2023-04-21 23:18:56 +0700

    play sound tester settings view

--- ExtendedPomodoro/Views/SettingsView.xaml.cs
+++ ExtendedPomodoro/Views/SettingsView.xaml.cs
@@ -1,7 +1,11 @@
-﻿using System;
+﻿using ExtendedPomodoro.Models.Domains;
+using ExtendedPomodoro.Services;
+using ExtendedPomodoro.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,9 +24,17 @@ namespace ExtendedPomodoro.Views
     /// </summary>
     public partial class SettingsView : Page
     {
+        private readonly SoundService _soundService = new();
         public SettingsView()
         {
             InitializeComponent();
+
+        }
+        private void VolumeIconOnlyButton_Click(object sender, RoutedEventArgs e)
+        {
+            _soundService.Volume = (int)VolumeSlider.Value;
+            _soundService.RepeatFor = TimeSpan.FromSeconds(3);
+            _soundService.Play((AlarmSound)AlarmSoundComboBox.SelectedValue);
         }
     }
 }
